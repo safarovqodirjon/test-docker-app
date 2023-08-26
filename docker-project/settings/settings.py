21dcf5e074c85242233ff61904b8abcefba718a7
safarov qodirjon <qodirjonsafaroff@gmail.com>
2023-08-26 12:29:17 +0500

    l

--- docker-project/settings/settings.py
+++ docker-project/settings/settings.py
@@ -119,5 +119,4 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-myapp.wsgi.application
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
