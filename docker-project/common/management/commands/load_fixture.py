ff818ca6b8671a72aab415f3086f49354b657093
safarov qodirjon <qodirjonsafaroff@gmail.com>
2023-08-26 11:14:00 +0500

    my commit -1

---
+++ docker-project/common/management/commands/load_fixture.py
@@ -0,0 +1,11 @@
+# from django.core.management.base import BaseCommand
+# from django.core.management import call_command
+# import os
+#
+#
+# class Command(BaseCommand):
+#     help = 'Loads the sellary_fixture data'
+#
+#     def handle(self, *args, **options):
+#         call_command('loaddata')
+#         self.stdout.write(self.style.SUCCESS('Fixture data loaded successfully.'))
